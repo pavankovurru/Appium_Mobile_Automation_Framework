ca839f4557a74c634b55127097282074ed6c6936
pavan Kovurru <pkovurru@phunware.com>
2017-09-18 11:11:49 -0500

    Add Property file read/write Utilities

--- src/main/java/com/company/project/utilities/PropertiesLoader.java
+++ src/main/java/com/company/project/utilities/PropertiesLoader.java
@@ -2,32 +2,53 @@ package com.company.project.utilities;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class PropertiesLoader {
-	
-	private Properties pro;
-	private FileInputStream fi=null;
-	
-	public PropertiesLoader(String path){
-		pro= new Properties();
+
+	private static Properties prop=null;
+	private static FileInputStream fi=null;
+	private static FileOutputStream fo=null;
+
+
+
+	public static void writePropertyFile(String key , String value, String propertyFilePath, String comments){
+
 		try {
-			fi= new FileInputStream(path);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} 
-		try {
-			pro.load(fi);
-		} catch (IOException e) {
-			e.printStackTrace();
+			prop= new Properties();
+			fo = new FileOutputStream(propertyFilePath);
+			prop.setProperty(key, value);
+			prop.store(fo, comments);
+			fo.close();
+		} catch (FileNotFoundException ex) {  // file does not exist
+			ex.printStackTrace();
+		} catch (IOException ex) {  // I/O error
+			ex.printStackTrace();
 		}
 	}
-	
-	
-	public String getProperty(String propertyName){
-		return pro.getProperty(propertyName);
+
+
+	public static String readDataFromPropertiesFile( String propertyName, String propertyFilePath){
+
+		try {
+			prop= new Properties();
+			fi = new FileInputStream(propertyFilePath);
+			prop.load(fi);
+		}
+
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return prop.getProperty(propertyName);
+
 	}
-	
+
+
 
 }
